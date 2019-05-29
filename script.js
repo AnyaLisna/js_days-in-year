88c83313378bdea9f1fbc9b106f166caaecb7043
AnyaLisna <anyalesnaya2812@gmail.com>
2019-05-29 23:06:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function daysInYear(year) {
+
+  try {
+
+    if(!Number.isInteger(year)) {
+    throw new Error (`Year isn't correct`);
+  } 
+    return (new Date(`${year+1}`) - new Date(`${year}`))/86400000;
+
+  } catch(e) {
+    return e.message;
+  }
+} 
+
+
+console.log(daysInYear(2019)); // 365
+console.log(daysInYear('2019')); // exception
+console.log(daysInYear(2020)); // 366
+console.log(daysInYear(2020.2)); // exception
+console.log(daysInYear(2100)); // 365
+console.log(daysInYear({year: 2100})); // exception
